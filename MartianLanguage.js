46ae65e0304dcf9e2b8072712e21f57666242c52
Tyler Tolson <tylertolson1@gmail.com>
2019-10-04 18:27:32 -0400

    optomized the js file

--- MartianLanguage.js
+++ MartianLanguage.js
@@ -1,15 +1,31 @@
 /* what I invented -- MAD LW SGOLARAS */
 $(() => {
-	let martianDicionary = '';
-	let englishDicionary = '';
-	let getRomanization = [];
 	let { Martian, English } = marsDitionary;
-	Martian = Martian.map(word => englishRomanization('Martian', word));
-	English = English.map(word => englishRomanization('English', word));
-	const sorter = (a, b) => (a < b ? -1 : a > b ? 1 : 0);
+	Martian.sort(sorter);
+	English.sort(sorter);
+	Martian = Martian.map(englishRomanization);
+	English = English.map(englishRomanization);
 
-	Martian = Martian.sort((a, b) => sorter(a.Martian, b.Martian));
-	English = English.sort((a, b) => sorter(a.English, b.English));
+	getMartianDictionary(Martian);
+	getEnglishDictionary(English);
+	getSpellingRules();
+
+	$('#martian-dicionary').show();
+	$('#english-dicionary').hide();
+
+	$('#show-martian').click(getToggle);
+	$('#show-english').click(getToggle);
+});
+
+function sorter(a, b) {
+	[a, b] = (a.IPA)
+		? [a.Martian, b.Martian]
+		: [a.English, b.English];
+	return (a < b) ? -1 : 1;
+};
+
+function getMartianDictionary(Martian) {
+	let martianDicionary = '';
 	$.each(Martian, (i, word) => {
 		martianDicionary += `<li>
 			<martian>${word.Martian}</martian> <b>|</b>
@@ -20,11 +36,12 @@ $(() => {
 		</li>`;
 	});
 	$('#martian-dicionary ul').html(martianDicionary);
+}
 
+function getEnglishDictionary(English) {
+	let englishDicionary = '';
 	$.each(English, (i, word) => {
-		if (word.English === 'i') {
-			word.English = 'I';
-		}
+		if (word.English === 'i') { word.English = 'I'; }
 		let wordGet = `<b>${word.English}</b> <b>|</b>
 		<pos>${word.POS}</pos> <b>|</b>
 		<martian>${word.Martian}</martian> <b>|</b>
@@ -41,18 +58,9 @@ $(() => {
 		englishDicionary += `<li> ${wordGet} </li>`;
 	});
 	$('#english-dicionary ul').html(englishDicionary);
+}
 
-	$('#martian-dicionary').show();
-	$('#english-dicionary').hide();
-
-	$('#show-martian').click(() => {
-		$('#martian-dicionary').show();
-		$('#english-dicionary').hide();
-	});
-	$('#show-english').click(() => {
-		$('#english-dicionary').show();
-		$('#martian-dicionary').hide();
-	});
+function getSpellingRules() {
 	let sounds = `<th> Sound </th>`;
 	let romanization = `<th> Romanization </th>`;
 	let letters = `<th> Letter </th>`;
@@ -61,69 +69,46 @@ $(() => {
 		romanization += `<td> ${word.romanization} </td>`;
 		letters += `<td> <martian>${word.letter}</martian> </td>`;
 	});
-	$('#spell-rules-data').html(
-		`<table> <tr>${letters}</tr> <tr>${romanization}</tr> <tr>${sounds}</tr> </table>`
-	);
+	$('#spell-rules-data').html(`<table> <tr>${letters}</tr> <tr>${romanization}</tr> <tr>${sounds}</tr> </table>`);
+}
 
-	function englishRomanization(language, englishWord) {
-		let getMartian = englishWord.Martian;
-		let getNew = '';
-		let getIPA = '';
-		for (let MartianLetter in getMartian) {
-			for (let soundLetter in soundSymbol) {
-				soundLetter = soundSymbol[soundLetter];
-				switch (getMartian[MartianLetter]) {
-					case soundLetter.letter:
-						getNew += soundLetter.romanization;
-						getIPA += soundLetter.sound;
-						break;
-					case ',':
-						getNew += ', ';
-						getIPA += ' ';
-						break;
-					case ' ':
-						getNew += ' ';
-						getIPA += ' ';
-						break;
-				}
-			}
-			if (/ci/g.test(getIPA)) {
-				getIPA = getIPA.replace(/ci/g, 'ki');
-			}
-			if (/kyi/g.test(getNew)) {
-				getNew = getNew.replace(/kyi/g, 'ki');
-			}
+function getToggle() {
+	$('#martian-dicionary').toggle();
+	$('#english-dicionary').toggle();
+}
 
-			if (/sŋ/g.test(getIPA)) {
-				getIPA = getIPA.replace(/sŋ/g, 'ŋ');
-			}
-			if (/sng/g.test(getNew)) {
-				getNew = getNew.replace(/sng/g, 'ng');
-			}
-
-			if (/ʃʒ/g.test(getIPA)) {
-				getIPA = getIPA.replace(/ʃʒ/g, 'ʒ');
-			}
-			if (/shzh/g.test(getNew)) {
-				getNew = getNew.replace(/shzh/g, 'zh');
-			}
-
-			// if (/c$/.test(getIPA)) { getIPA = getIPA.replace(/c$/, 'k'); }
-			// if (/ky$/.test(getNew)) { getNew = getNew.replace(/ky$/, 'k'); }
-			// if (/ɲ$/.test(getIPA)) { getIPA = getIPA.replace(/ɲ$/, 'ŋ'); }
-			// if (/ny$/.test(getNew)) { getNew = getNew.replace(/ny$/, 'ng'); }
-
-			if (/(, )+/g.test(getNew)) {
-				getNew = getNew.replace(/(, )+/g, ', ');
-			}
-			if (/ +/g.test(getNew)) {
-				getNew = getNew.replace(/ +/g, ' ');
+function englishRomanization(getWord) {
+	let getMartian = getWord.Martian;
+	let getNew = '';
+	let getIPA = '';
+	for (let MartianLetter in getMartian) {
+		for (let soundLetter in soundSymbol) {
+			soundLetter = soundSymbol[soundLetter];
+			switch (getMartian[MartianLetter]) {
+				case soundLetter.letter:
+					getNew += soundLetter.romanization;
+					getIPA += soundLetter.sound;
+					break;
+				case ',': getNew += ', '; getIPA += ' '; break;
+				case ' ': getNew += ' '; getIPA += ' '; break;
 			}
 		}
-		return {
-			...englishWord,
-			romanization: getNew,
-			IPA: `/${getIPA}/`
-		};
+		if (/ci/g.test(getIPA)) { getIPA = getIPA.replace(/ci/g, 'ki'); }
+		if (/kyi/g.test(getNew)) { getNew = getNew.replace(/kyi/g, 'ki'); }
+
+		if (/sŋ/g.test(getIPA)) { getIPA = getIPA.replace(/sŋ/g, 'ŋ'); }
+		if (/sng/g.test(getNew)) { getNew = getNew.replace(/sng/g, 'ng'); }
+
+		if (/ʃʒ/g.test(getIPA)) { getIPA = getIPA.replace(/ʃʒ/g, 'ʒ'); }
+		if (/shzh/g.test(getNew)) { getNew = getNew.replace(/shzh/g, 'zh'); }
+
+		// if (/c$/.test(getIPA)) { getIPA = getIPA.replace(/c$/, 'k'); }
+		// if (/ky$/.test(getNew)) { getNew = getNew.replace(/ky$/, 'k'); }
+		// if (/ɲ$/.test(getIPA)) { getIPA = getIPA.replace(/ɲ$/, 'ŋ'); }
+		// if (/ny$/.test(getNew)) { getNew = getNew.replace(/ny$/, 'ng'); }
+
+		if (/(, )+/g.test(getNew)) { getNew = getNew.replace(/(, )+/g, ', '); }
+		if (/ +/g.test(getNew)) { getNew = getNew.replace(/ +/g, ' '); }
 	}
-});
+	return { ...getWord, romanization: getNew, IPA: `/${getIPA}/` };
+}
